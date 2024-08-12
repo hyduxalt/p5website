8c15f44ad63155671e31213eb1e4cda7c467832b
hyduxalt <85044595+hyduxalt@users.noreply.github.com>
2024-08-12 20:31:01 +0800

    Add files via upload

---
+++ ver2/map/map.js
@@ -0,0 +1,87 @@
+const overpassURL = "https://overpass-api.de/api/interpreter?data=" 
+const geonamesAPI = "https://api.3geonames.org/?randomland=yes&"
+
+let highwaysData;
+let geoData;
+let nodesMap = {};
+const coord = "37.8067898,-122.4112719"
+let minLat, maxLat, minLon, maxLon;
+
+function miToDegLat(mi) {
+  return mi / 69.172;
+}
+
+function miToDegLon(mi, lat) {
+  return mi / (69.172 * Math.cos(radians(lat)));
+}
+
+async function preload() {
+  // api call to get random coordinates
+  geoData = await fetch(geonamesAPI);
+  console.log(geoData);
+
+  let w = 2; // viewport width in miles
+  let h = windowHeight * 2 / windowWidth; 
+
+  let centerLat = parseFloat(coord.split(",")[0]);
+  let centerLon = parseFloat(coord.split(",")[1]);
+
+  // bbox coordinates
+  minLat = centerLat - miToDegLat(h) / 2;
+  maxLat = centerLat + miToDegLat(h) / 2;
+  minLon = centerLon - miToDegLon(w, centerLat) / 2;
+  maxLon = centerLon + miToDegLon(w, centerLat) / 2;
+
+  console.log(minLat, maxLat, minLon, maxLon);
+
+  // overpass ql query
+  let query = `[out:json];
+    (
+      way["highway"](bbox:${minLat},${minLon},${maxLat},${maxLon});
+      node(w);
+    );
+    out body;`;
+
+  // HTTP request to overpass API
+  loadJSON(overpassURL + encodeURIComponent(query), processData);
+}
+
+function processData(data) {
+  highwaysData = data;
+  // Build nodes map for faster lookup
+  for (let node of highwaysData.elements) {
+    if (node.type === "node") {
+      nodesMap[node.id] = node;
+    }
+  }
+}
+
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+	canvas.parent('sketch');
+
+  background(backgroundColor.r, backgroundColor.g, backgroundColor.b);
+  if (highwaysData) {
+    drawHighways();
+  }
+}
+  
+function drawHighways() {
+  for (let way of highwaysData.elements) {
+    if (way.type === "way" && way.nodes.length > 1) {
+      stroke(outlineColor);
+      strokeWeight(2);
+      noFill();
+      beginShape();
+      for (let nodeId of way.nodes) {
+        let node = nodesMap[nodeId];
+        if (node) {
+          let x = map(node.lon, minLon, maxLon, 0, width);
+          let y = map(node.lat, maxLat, minLat, 0, height);
+          vertex(x, y);
+        }
+      }
+      endShape();
+    }
+  }
+}
